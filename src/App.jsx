7fcb0bc2de9cd9c7b392fe23aff807a8edb31187
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-03-30 06:57:30 +0000

    Login sin conectar al back

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import FooterSection from './components/commons/FooterSection.jsx';
 import HeaderSection from './components/commons/HeaderSection.jsx';
 import Homepage from "./pages/Homepage.jsx";
+import Login from "./pages/login/Login.jsx"
 import DisponiblesPage from "./pages/servicios/DisponiblesPage.jsx";
+import React, { useState } from 'react';
 
 
 function App() {
 
-
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
         <BrowserRouter future={{ v7_startTransition: true }}>
             <HeaderSection />
             <div className='homepage-container-web'>
                 <Routes>
-                    <Route path='/' element={<Homepage />} />
+                    <Route path='/login' element={ isLoggedIn ? <Homepage /> : <Login setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route path='/' element={ isLoggedIn ? <Homepage /> : <Navigate to='/login' />} />
                     <Route path='/servicios/disponibles' element={<DisponiblesPage />} />
                 </Routes>
             </div>
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+} from 'mdb-react-ui-kit';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+function Login({setIsLoggedIn}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+ const handleLogin = () => {
+  setIsLoggedIn(true)
+ }
+
+  return (
+    <div className="container-fluid p-0">
+      <div
+        className="d-flex justify-content-end align-items-center"
+        style={{
+          backgroundImage: 'url(https://img.freepik.com/free-photo/bearded-builder-man-construction-uniform-safety-helmet-wearing-rubber-gloves-holding-paint-roller-looking-camera-happy-confident-showing-ok-sign-standing-purple-background_141793-110835.jpg?t=st=1711774801~exp=1711778401~hmac=f3ead3bf588126fd60cf4d6a19b1d95c4c0a968d92bf5c380b9c4edb36b78874&w=1060)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100%',
+          height: '85vh',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: "purple",
+        }}
+      >
+        <div className="container p-3" style={{ height: '80vh' }}>
+          <div className="row" style={{ height: '80%' }}>
+            <div className="col-md-6"></div>
+            <div className="col-md-6 container-login d-flex justify-content-end align-items-center p-5">
+              <MDBCardBody style={{ backgroundColor: 'rgba(255, 255, 255, 0.55)', height: '100%' }} className="d-flex flex-column justify-content-center align-items-center rounded">
+                <div className="d-flex flex-row mt-2">
+                  <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
+                  <span className="h4 fw-bold mt-2 justify-content-center align-items-center">HOGAR PRO SOLUTIONS</span>
+                </div>
+
+                <MDBInput wrapperClass='mb-2 mt-4' label='Email address' id='formControlLg' type='email' size="md"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+                <MDBInput wrapperClass='mb-2' label='Password' id='formControlLg' type='password' size="md"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} />
+
+                <Button className="mb-2 px-5" color='dark' size='sm' onClick={handleLogin}>Login</Button>
+                <a className="small text-muted" href="#!">Forgot password?</a>
+                <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a></p>
+              </MDBCardBody>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
